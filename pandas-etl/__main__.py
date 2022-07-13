72f7030683113ca5a0297fa7992296687a8d2e33
msuthar09 <maharshi.suthar@righteous.ai>
2022-07-13 14:55:16 -0400

    IN progress

--- pandas-etl/__main__.py
+++ pandas-etl/__main__.py
@@ -1,22 +1,49 @@
 from . import etl
-
+import sys
 import argparse
 
-parser = argparse.ArgumentParser(
-    prog="pandas-etl",
-    # usage='',
-    description="Process some integers.",
-    # epilog='',
-)
 
-parser.add_argument("-f", "--file", nargs="*", help="Path to yaml config file")
+def cl_args_parser():
+
+    parser = argparse.ArgumentParser(
+        prog="pandas-etl",
+        # usage='',
+        description="A utility for running YAML config as Pandas based ETL code",
+        epilog="For Example: python -m pandas-etl --file '<YAML_FILE_PATH>' --var varName1=varValue1 varName2=varValue2 --var varName3=varValue3 ...",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        nargs="*",
+        help="Path to yaml config file",
+        required=True,
+        metavar="'usr/dir/file.yaml'",
+    )
+    parser.add_argument(
+        "-v",
+        "--var",
+        action="extend",
+        nargs="*",
+        help="Define new variables or overwrite existing variables",
+        metavar="varName1=varValue1",
+    )
+
+    return parser
+
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    filePaths = args.file
-    yaml_file_objects = etl.open_yaml_file(filePaths)
+    parser = cl_args_parser()
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        sys.exit(0)
+
+    yaml_file_objects = etl.open_yaml_file(args)
 
     for obj in yaml_file_objects:
-        yamlData = etl.find_and_replace_variables(obj)
+        yamlData = etl.add_argument_variables(args, obj)
+        yamlData = etl.find_and_replace_variables(yamlData)
         etl.find_and_execute_script(yamlData)
         etl.execute_steps(yamlData)
--- pandas-etl/etl.py
+++ pandas-etl/etl.py
@@ -1,17 +1,18 @@
+from argparse import Namespace
 import os
 import sys
-from click import FileError
 import yaml
-from calltraces.linetrace import traceInfo
+from calltraces.linetrace import traceInfo, traceError
 from calltraces.functiontrace import functiontrace
-import tqdm
+from tqdm.auto import tqdm
 import re
-import argparse
+from sqlalchemy import create_engine
 
 
 @functiontrace
-def open_yaml_file(filePaths: list) -> list:
+def open_yaml_file(args: Namespace) -> list:
     """Open a YAML file from the specified file path"""
+    filePaths = args.file
     try:
         data = []
         for i in range(len(filePaths)):
@@ -20,7 +21,7 @@ def open_yaml_file(filePaths: list) -> list:
                     with open(filePaths[i]) as f:
                         data.append(yaml.load(f, Loader=yaml.FullLoader))
                 else:
-                    raise FileError("Wrong file extension")
+                    raise ValueError("Wrong file extension")
             else:
                 raise FileNotFoundError("No such file")
         return data
@@ -28,21 +29,34 @@ def open_yaml_file(filePaths: list) -> list:
         raise ValueError("Wrong filePaths input")
 
 
+@functiontrace
+def add_argument_variables(args, yamlData: dict) -> dict:
+    """This function adds variables defined in the arguments to the yaml file variables"""
+    if args.var is not None:
+        if "variables" not in yamlData.keys():
+            yamlData["variables"] = {}
+        for v in args.var:
+            argKey, argValue = v.split("=")
+            for k, v in list(yamlData.get("variables", {}).items()):
+                if k == argKey:
+                    k = argValue
+            yamlData["variables"][argKey] = argValue
+    return yamlData
+
+
+@functiontrace
 def find_and_replace_variables(yamlData: dict) -> dict:
     """This function helps find and replace variables"""
-    import re
-    import yaml
-
     fieldValue = yaml.dump(yamlData)
     yamlVariables = yamlData.get("variables", {})
-    variables_regex = re.compile(r"\{\{var\.(\w+)\}\}")
+    variables_regex = re.compile(r"\{\{ var\.(.*?) \}\}")
     variables_matched = re.findall(pattern=variables_regex, string=fieldValue)
     for v in variables_matched:
         if v not in yamlVariables.keys():
             raise ValueError(f"Unknown variable {v} found")
         else:
             fieldValue = re.sub(
-                pattern=r"\{\{var\." + v + r"\}\}",
+                pattern=r"\{\{ var\." + v + r" \}\}",
                 repl=yamlVariables[v],
                 string=fieldValue,
             )
@@ -50,77 +64,22 @@ def find_and_replace_variables(yamlData: dict) -> dict:
     return output
 
 
+@functiontrace
+def create_engine_connection(yamlData: dict):
+    """This function will find and create engine connection"""
+    if "connections" in yamlData.keys():
+        for conn in yamlData.get("connections", {}):
+            conn["engine"] = create_engine(conn["connStr"])
+
+
+@functiontrace
 def find_and_execute_script(yamlData: dict):
     """This function will find and execute script under pre-flight"""
     script = yamlData.get("pre-flight", {}).get("script", "")
     exec(script, globals())
 
 
-def find_and_execute_all_steps(yamlData: dict):
-    """This function will find and execute all steps"""
-    for step in yamlData["steps"]:
-        print(
-            f"Step name: {step['name']}; Step description: {step.get('description', 'No description provided')}"
-        )
-        output = eval(f"{step['function']}('{step['args'][0]}')")
-        step["output"] = output
-
-
-def find_and_replace_step_output(yamlData: dict) -> dict:
-    """This function finds and replaces the step output"""
-    import yaml
-    import re
-
-    fieldValue = yaml.dump(yamlData)
-    steps_output_regex = re.compile(r"\{\{steps\[\'(.*?)\'\]\.output\}\}")
-    steps_output_matched = re.findall(steps_output_regex, string=fieldValue)
-    for s in steps_output_matched:
-        for x in range(len(yamlData["steps"])):
-            try:
-                if yamlData["steps"][x]["name"] == s:
-                    fieldValue = re.sub(
-                        pattern=r"\{\{steps\[\'" + s + r"\'\]\.output\}\}",
-                        repl=yamlData["steps"][x]["output"],
-                        string=fieldValue,
-                    )
-            except:
-                raise ValueError(f"NO step output found for step name: {s}")
-    output = yaml.load(fieldValue, Loader=yaml.FullLoader)
-    return output
-
-
 @functiontrace
-def execute_steps(yamlData: dict):
-    """This function executes the steps specified"""
-    for step in yamlData["steps"]:
-        print(
-            f"Step name: {step['name']}; Step description: {step.get('description', 'No description provided')}"
-        )
-
-        # Check if the step requires output from the previous steps.
-        steps_output_regex = re.compile(r"\{\{steps\[\'(.*?)\'\]\.output\}\}")
-        fieldValue = yaml.dump(step)
-        steps_output_matched = re.findall(steps_output_regex, string=fieldValue)
-        for s in steps_output_matched:
-            for x in range(len(yamlData["steps"])):
-                try:
-                    if yamlData["steps"][x]["name"] == s:
-                        find_key_by_value_and_assign(
-                            step,
-                            "{{steps['" + s + "'].output}}",
-                            yamlData["steps"][x]["output"],
-                        )
-                except:
-                    raise ValueError(f"NO step output found for step name: {s}")
-
-        # Find the correct function and get output
-        output = eval(f"{step['function']}('{step['args'][0]}')")
-
-        # Assign the output of the function to the step output
-        step["output"] = output
-
-
-# Assisting Function for Function execute_steps
 def find_key_by_value_and_assign(data: dict, target: str, assign):
     """This function finds the key by value and assigns a new value to the key"""
     for k, v in data.items():
@@ -132,3 +91,107 @@ def find_key_by_value_and_assign(data: dict, target: str, assign):
             for i in v:
                 if isinstance(i, dict):
                     return find_key_by_value_and_assign(i, target, assign)
+
+
+@functiontrace
+def replace_steps_output(
+    steps_output_matched: list, step: dict, yamlData: dict
+) -> dict:
+    """This function replaces the steps key value pairs with output"""
+    for s in steps_output_matched:
+        for x in range(len(yamlData["steps"])):
+            try:
+                if yamlData["steps"][x]["name"] == s:
+                    find_key_by_value_and_assign(
+                        step,
+                        "{{ steps['" + s + "'].output }}",
+                        yamlData["steps"][x]["output"],
+                    )
+            except:
+                raise ValueError(f"NO step output found for step name: {s}")
+    return step
+
+
+@functiontrace
+def resolve_connections_variables(
+    conn_matched: list, step: dict, yamlData: dict
+) -> dict:
+    """This function helps resolve connection variables for sql queries"""
+    for c in conn_matched:
+        for x in range(len(yamlData["connections"])):
+            try:
+                if yamlData["connections"][x]["name"] == c:
+                    find_key_by_value_and_assign(
+                        step,
+                        "{{ conn." + c + " }}",
+                        yamlData["connections"][x]["engine"],
+                    )
+            except:
+                raise ValueError(f"NO connection engine found for name: {c}")
+    return step
+
+
+@functiontrace
+def execute_steps(yamlData: dict):
+    """This function executes the steps specified"""
+    tqdm_function_list = tqdm(
+        iterable=yamlData["steps"],
+        unit=" function",
+        desc="YAML Steps",
+    )
+    for step in tqdm_function_list:
+        tqdm_function_list.set_postfix_str(
+            "Function Name: "
+            + step["name"]
+            + "; Function Description: "
+            + step.get("description", "No description provided")
+        )
+        fieldValue = yaml.dump(step)
+
+        # Check if the step requires output from the previous steps
+        steps_output_regex = re.compile(r"\{\{ steps\[\'(.*?)\'\]\.output \}\}")
+        steps_output_matched = re.findall(steps_output_regex, string=fieldValue)
+        if len(steps_output_matched) > 0:
+            step = replace_steps_output(steps_output_matched, step, yamlData)
+
+        # Check if the step execution requires connections engine to the database
+        conn_regex = re.compile(r"\{\{ conn\.(.*) \}\}")
+        conn_matched = re.findall(conn_regex, string=fieldValue)
+        if len(conn_matched) > 0:
+            step = resolve_connections_variables(conn_matched, step, yamlData)
+
+        # Find the correct function and get output
+        output = eval(f"{step['function']}('{step['args'][0]}')")
+
+        # Assign the output of the function to the step output
+        step["output"] = output
+
+
+# def find_and_execute_all_steps(yamlData: dict):
+#     """This function will find and execute all steps"""
+#     for step in yamlData["steps"]:
+#         print(
+#             f"Step name: {step['name']}; Step description: {step.get('description', 'No description provided')}"
+#         )
+#         output = eval(f"{step['function']}('{step['args'][0]}')")
+#         step["output"] = output
+
+
+# def find_and_replace_step_output(yamlData: dict) -> dict:
+#     """This function finds and replaces the step output"""
+#     fieldValue = yaml.dump(yamlData)
+#     steps_output_regex = re.compile(r"\{\{ steps\[\'(.*?)\'\]\.output \}\}")
+#     steps_output_matched = re.findall(steps_output_regex, string=fieldValue)
+#     for s in steps_output_matched:
+#         for x in range(len(yamlData["steps"])):
+#             try:
+#                 if yamlData["steps"][x]["name"] == s:
+#                     fieldValue = re.sub(
+#                         pattern=r"\{\{ steps\[\'" + s + r"\'\]\.output \}\}",
+#                         repl=yamlData["steps"][x]["output"],
+#                         string=fieldValue,
+#                     )
+#             except:
+#                 raise ValueError(f"NO step output found for step name: {s}")
+#     output = yaml.load(fieldValue, Loader=yaml.FullLoader)
+#     return output
